098d77150792c7a5e5b586b1b096ef388d0fd95b
Rinku K Kaneria <rinkubathani@gmail.com>
2021-07-28 19:47:33 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,16 +1,33 @@
 import geni.portal as portal
 import geni.rspec.pg as pg
+import geni.rspec.igext as IG
 
 pc = portal.Context()
 request = pc.makeRequestRSpec()
- 
-# Add a raw PC to the request.
-node = request.XenVM("node")
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-node.routable_control_ip = "true"
 
-# Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
+tourDescription = \
+"""
+This profile provides a two-node for assignment-2. One node will be the Apache Webserver, and the other node is a client for NFS mount. 
+"""
 
+tour = IG.Tour()
+tour.Description(IG.Tour.TEXT,tourDescription)
+request.addTour(tour)
+prefixForIP = "192.168.1."
+link = request.LAN("lan")
+
+for i in range(2):
+  if i == 0:
+    node = request.XenVM("webserver")    
+  else:
+    node = request.XenVM("observer")
+   
+  node.routable_control_ip = "true"  
+  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+  iface = node.addInterface("if" + str(i))
+  iface.component_id = "eth1"
+  iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
+  link.addInterface(iface)
+  
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
